3512cece29da67311c3f1a8ada1c34db865bb8b4
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-05-27 14:46:35 -0400

    updated the the script for running fizzy

--- src/run-study-550.sh
+++ src/run-study-550.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash 
 
 # set up the data paths 
-biom_fp=../data/study_550_closed_reference_otu_table.biom
-map_fp=../data/study_550_mapping_file.txt
+biom_fp=../data/study_550_closed_reference_otu_table_feces.biom
+map_fp=../data/mapping_feces.txt
 out_fp=output-550.txt
 labels=SEX
 method=MIM
-n_select=100
+n_select=10
 
 ./fizzy -i $biom_fp -m $map_fp -o $out_fp -l $labels -f $method -n $n_select 
